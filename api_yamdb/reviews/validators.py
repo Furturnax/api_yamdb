bb1c160c20e5526d97f61d6343f9f4fa6338696f
Andrew Fedorchenko <furturnax@gmail.com>
2023-12-13 19:56:09 +0300

    Fix validator

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -8,14 +8,14 @@ from rest_framework.exceptions import ValidationError
 
 def username_validator(value):
     """Валидатор юзернейна."""
-    regex = r"^[\w.@+-]+\Z"
+    regex = r'^[\w.@+-]+\Z'
     if re.search(regex, value) is None:
-        invalid_characters = set(re.findall(r"[^\w.@+-]", value))
+        invalid_characters = set(re.findall(r'[^\w.@+-]', value))
         raise ValidationError(
             (
-                f"Не допустимые символы {invalid_characters} в username. "
-                f"username может содержать только буквы, цифры и "
-                f"знаки @/./+/-/_."
+                f'Не допустимые символы {invalid_characters} в username. '
+                f'username может содержать только буквы, цифры и '
+                f'знаки @/./+/-/_.'
             ),
         )
     if value.lower() == settings.CANT_USED_IN_USERNAME:
